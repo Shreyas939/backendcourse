49788770f0881e4a1cf86cb8ca31d190565885a3
Shreyas939 <shreyasnaik5454@gmail.com>
2024-12-26 18:45:52 +0530

    refreshaccesstoken

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import {registerUser} from '../controllers/user.controller.js'
+import {loginUser ,registerUser,logOutUser,refreshAccessToken} from '../controllers/user.controller.js'
 import {upload} from "../middlewares/multer.middleware.js"
-
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 // same as express app 
 const router = Router()
 
 router.route("/register").post(
-    upload.fields([
+    upload.fields([  // this is our middleware
         {
             name:"avatar",
             maxCount:1
@@ -20,7 +20,12 @@ router.route("/register").post(
     registerUser
 )
 
+router.route("/login").post(loginUser)
 
+
+//secured routes
+router.route("/logout").post(verifyJWT, logOutUser) // that's why we write next() so that run next method after first one
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 export default router
